2c4b5bd6c95a3a91a11e8b2e4f5f2f7063e72dfc
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2019-02-12 23:31:45 +0100

    Remove Library so the patch is applied only once

--- get_frameworks.sh
+++ get_frameworks.sh
@@ -16,8 +16,9 @@ echo "Downloading header file:"
 curl -OL $HHROOT/ios_system/releases/download/v$IOS_SYSTEM_VER/ios_error.h 
 
 echo "Downloading python files:"
+rm -rf "${PWD}/Library"
 mkdir -p "${PWD}/Library"
 (cd "${PWD}/Library" ; curl -OL https://github.com/holzschu/python3_ios/releases/download/v1.0/release.tar.gz ; tar xzf release.tar.gz ; rm release.tar.gz)
-echo "Patching Javscript files:"
+echo "Patching Javascript files:"
 (cd "${PWD}/Library" ;  patch -p1 < ../Library.patch ; cd ..)
 
