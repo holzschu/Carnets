99b9cbc23edc4e7b4e2e9c79965a620439cbc31e
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2021-02-03 21:23:00 +0100

    Updated get_frameworks.sh for work with swift + xcfs

--- get_frameworks.sh
+++ get_frameworks.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
-IOS_SYSTEM_VER="2.4"
-HHROOT="https://github.com/holzschu"
-
-mkdir -p "${PWD}/Frameworks"
-(cd "${PWD}/Frameworks"
-# ios_system
-echo "Downloading ios_system.framework and associated dylibs"
-curl -OL $HHROOT/ios_system/releases/download/v$IOS_SYSTEM_VER/release.tar.gz
-( tar -xzf release.tar.gz --strip 1 && rm release.tar.gz ) || { echo "ios_system failed to download"; exit 1; }
-)
-# Remove x86 & i386 archs from frameworks, if present:
-./fix-framework-archs.sh
-echo "Downloading header file:"
-curl -OL $HHROOT/ios_system/releases/download/v$IOS_SYSTEM_VER/ios_error.h 
-
-
+swift run --package-path xcfs
